25c8c6634cfafb83d9d9c06a8d17137480d25a2e
까망베르냥 <cute.lovely.cat@kakao.com>
2022-03-28 16:19:15 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"; 
+
+const Father = styled.div`
+  display: fles; 
+  color: #ef4056;
+`;
+const Box = styled.div`
+  background: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border:0;
+  padding: 20px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father as="header">
+      <Box bgColor="teal" />
+      <Circle bgColor="blue" />
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+    </Father>
   );
 }
 
